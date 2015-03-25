13508603a35976e6020fb66591ce5146e6cb81f4
林盛 <linren_888@126.com>
2015-03-25 16:15:58 +0800

    新增按钮组插件

--- jquery.easyui-ext.js
+++ jquery.easyui-ext.js
@@ -1,9 +1,23 @@
-(function($){
+(function ($) {
     var boxes = ['searchbox', 'textbox', 'combo', 'combobox', 'datebox', 'filebox', 'combogrid', 'combotree',
         'datetimebox', 'datetimespinner', 'numberbox', 'numberspinner', 'timespinner'];
-    for(var i in boxes){
+    for (var i in boxes) {
         $.fn[boxes[i]].defaults.height = 34;
     }
     $.fn.tabs.defaults.tabHeight = 38;
     $.fn.datebox.defaults.panelWidth = $.fn.datetimebox.defaults.panelWidth = 210;
+    //新增按钮组插件
+    $.fn.buttonGroup = function (options) {
+        $(this).each(function (i) {
+            $(this).addClass('l-btn-group').children().each(function () {
+                $(this).linkbutton($.extend({
+                    group: 'g' + i,
+                    toggle: true
+                }, options));
+            });
+        });
+    };
 })(jQuery);
+$(function () {
+    $('.easyui-button-group').buttonGroup();
+});
